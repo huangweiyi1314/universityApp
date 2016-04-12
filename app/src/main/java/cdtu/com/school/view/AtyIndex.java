a17b432cd079305edf547702ed7959a3fb095f96
huangweiyi1314 <huangjielwy@foxmail.com>
2016-04-12 21:15:02 +0800

    编写IAtyIndex

--- app/src/main/java/cdtu/com/school/view/AtyIndex.java
+++ app/src/main/java/cdtu/com/school/view/AtyIndex.java
@@ -2,7 +2,6 @@ package cdtu.com.school.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 /**
  * Created by huangjie on 2016/4/12.
  */
--- app/src/main/java/cdtu/com/school/view/IAtyIndex.java
+++ app/src/main/java/cdtu/com/school/view/IAtyIndex.java
@@ -4,5 +4,5 @@ package cdtu.com.school.view;
  * Created by huangjie on 2016/4/12.
  */
 public interface  IAtyIndex {
-
+    public void  showLocationSchool();
 }
