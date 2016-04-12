5e2116cad64b7a7dc9121702445a745241ebb3c0
huangweiyi1314 <huangjielwy@foxmail.com>
2016-04-12 22:39:24 +0800

    替换图标

--- app/src/main/java/cdtu/com/school/view/IAtyIndex.java
+++ app/src/main/java/cdtu/com/school/view/IAtyIndex.java
@@ -3,7 +3,13 @@ package cdtu.com.school.view;
 /**
  * Created by huangjie on 2016/4/12.
  */
-public interface  IAtyIndex {
-    public void  showLocationSchool();
-    public void   toStartOtherAty();
+public interface IAtyIndex {
+    //打开定位框
+    public void showLocationSchool();
+
+    //打开其它acitivity
+    public void toStartOtherAty();
+
+    public void changeAdapter();
+
 }
